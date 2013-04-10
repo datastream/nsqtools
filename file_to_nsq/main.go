ec84241a0fb7d92ddd94bf4c78c7b07194224147
datastream <guxianjie@gmail.com>
2013-04-10 16:59:11 +0800

    record read offset every 10 sec or exit

--- file_to_nsq/main.go
+++ file_to_nsq/main.go
@@ -4,13 +4,16 @@ import (
 	"bufio"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/datastream/nsq/nsq"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+	"time"
 )
 
 var (
@@ -30,13 +33,42 @@ func main() {
 		log.Fatal("fail to read config", err)
 	}
 	exitchan := make(chan int)
+	offset := read_stat(setting)
 	for k, v := range setting {
-		go read_log(v, k, w, exitchan)
+		go read_log(v, offset[k], k, w, exitchan)
 	}
 	termchan := make(chan os.Signal, 1)
 	signal.Notify(termchan, syscall.SIGINT, syscall.SIGTERM)
 	<-termchan
 	close(exitchan)
+}
+
+func read_stat(setting map[string]string) map[string]int64 {
+	stat := make(map[string]int64)
+	for k, _ := range setting {
+		stat_file, err := os.Open(k)
+		if err != nil {
+			stat[k] = 0
+			continue
+		}
+		s, err := ioutil.ReadAll(stat_file)
+		if err != nil {
+			stat[k] = 0
+			continue
+		}
+		i, err := strconv.ParseInt(string(s), 10, 64)
+		stat[k] = i
+	}
+	return stat
+}
+
+func sync_stat(stat string, value int64) {
+	fd, err := os.Create(stat)
+	if err != nil {
+		log.Println("fail to create ", stat, err)
+	}
+	defer fd.Close()
+	fd.WriteString(fmt.Sprintf("%d", value))
 }
 
 func ReadConfig(file string) (map[string]string, error) {
@@ -53,7 +85,7 @@ func ReadConfig(file string) (map[string]string, error) {
 	return setting, nil
 }
 
-func read_log(file string, topic string, w *nsq.Writer, exitchan chan int) {
+func read_log(file string, offset int64, topic string, w *nsq.Writer, exitchan chan int) {
 	fd, err := os.Open(file)
 	if err != nil {
 		log.Println(err)
@@ -61,44 +93,45 @@ func read_log(file string, topic string, w *nsq.Writer, exitchan chan int) {
 	}
 	defer fd.Close()
 	reader := bufio.NewReader(fd)
+	tick := time.Tick(time.Second * 10)
 	for {
-		select {
-		case <-exitchan:
-			return
-		default:
-			line, err := reader.ReadString('\n')
-			if err == io.EOF {
-				size0, er := fd.Seek(0, 2)
-				if er != nil {
-					log.Println(er)
-					return
-				}
-				fd, err = os.Open(file)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				size1, er := fd.Seek(0, 2)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				if size1 != size0 {
-					fd.Seek(0, 0)
-					reader = bufio.NewReader(fd)
-				}
-				continue
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			size0, er := fd.Seek(0, 1)
+			if er != nil {
+				log.Println(er)
+				return
 			}
+			fd, err = os.Open(file)
 			if err != nil {
 				log.Println(err)
 				return
 			}
-			cmd := nsq.Publish(topic, []byte(line))
-			_, _, err = w.Write(cmd)
+			_, err = fd.Seek(size0, 0)
 			if err != nil {
-				log.Println("NSQ writer", err)
-				w.ConnectToNSQ(*nsq_address)
+				fd.Seek(0, 0)
 			}
+			reader = bufio.NewReader(fd)
+			continue
+		}
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		cmd := nsq.Publish(topic, []byte(line))
+		_, _, err = w.Write(cmd)
+		if err != nil {
+			log.Println("NSQ writer", err)
+			w.ConnectToNSQ(*nsq_address)
+		}
+		select {
+		case <-tick:
+			size, _ := fd.Seek(0, 1)
+			sync_stat(topic, size)
+		case <-exitchan:
+			size, _ := fd.Seek(0, 1)
+			sync_stat(topic, size)
+			return
 		}
 	}
 }
