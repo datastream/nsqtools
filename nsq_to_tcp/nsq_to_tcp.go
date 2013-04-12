c4e3bcbeba838613c350929cce1dca228465b8dc
datastream <guxianjie@gmail.com>
2013-04-12 14:35:45 +0800

    add support mutil topic

--- nsq_to_tcp/nsq_to_tcp.go
+++ nsq_to_tcp/nsq_to_tcp.go
@@ -1,37 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"github.com/bitly/nsq/nsq"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 )
 
 var (
-	topic            = flag.String("topic", "", "nsq topic")
-	channel          = flag.String("channel", "nsq_to_tcp", "nsq channel")
-	maxInFlight      = flag.Int("max-in-flight", 200, "max number of messages to allow in flight")
-	lookupdHTTPAddrs = flag.String("lookupd-http-address", "127.0.0.1:4161", "lookupd http")
-	port             = flag.String("port", ":1514", "log send port")
+	conf_file = flag.String("conf", "config.json", "config file")
+	port      = flag.String("port", ":1514", "log send port")
 )
 
 type Msg struct {
-	Body []byte
-	Stat chan error
+	Topic string
+	Body  []byte
+	Stat  chan error
 }
 type MsgHandler struct {
+	Topic    string
 	msg_chan chan Msg
 }
 
 func (this *MsgHandler) HandleMessage(m *nsq.Message) error {
 	msg := Msg{
-		Body: m.Body,
-		Stat: make(chan error),
+		Topic: this.Topic,
+		Body:  m.Body,
+		Stat:  make(chan error),
 	}
 	this.msg_chan <- msg
 	return <-msg.Stat
@@ -39,44 +42,62 @@ func (this *MsgHandler) HandleMessage(m *nsq.Message) error {
 
 func main() {
 	flag.Parse()
-
-	if *topic == "" || *channel == "" {
-		log.Fatalf("--topic and --channel are required")
-	}
-
-	if *maxInFlight < 0 {
-		log.Fatalf("--max-in-flight must be > 0")
-	}
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-	r, err := nsq.NewReader(*topic, *channel)
+	setting, err := ReadConfig(*conf_file)
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatal("fail to read config", err)
 	}
-	r.SetMaxInFlight(*maxInFlight)
-	msg_handler := MsgHandler{make(chan Msg)}
-	r.AddHandler(&msg_handler)
-	lookupdlist := strings.Split(*lookupdHTTPAddrs, ",")
-	exitchan := make(chan int)
-	go tcp_server(*port, msg_handler.msg_chan, exitchan)
-	for _, addrString := range lookupdlist {
-		log.Printf("lookupd addr %s", addrString)
-		err := r.ConnectToLookupd(addrString)
+
+	topics := strings.Split(setting["topics"], ",")
+	maxInFlight, err := strconv.Atoi(setting["maxinflight"])
+	if err != nil {
+		maxInFlight = 200
+	}
+	msg_chan := make(chan Msg)
+	var reader_list []*nsq.Reader
+	for _, topic := range topics {
+		r, err := nsq.NewReader(topic, setting["channel"])
 		if err != nil {
 			log.Fatalf(err.Error())
 		}
+		r.SetMaxInFlight(maxInFlight)
+		msg_handler := MsgHandler{topic, msg_chan}
+		r.AddHandler(&msg_handler)
+		lookupdlist := strings.Split(setting["lookupdHTTPAddrs"], ",")
+		for _, addrString := range lookupdlist {
+			log.Printf("lookupd addr %s", addrString)
+			err := r.ConnectToLookupd(addrString)
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+		}
+		reader_list = append(reader_list, r)
 	}
-
-	select {
-	case <-r.ExitChan:
-		log.Println("reader exited")
-	case <-sigChan:
+	exitchan := make(chan int)
+	go tcp_server(setting["port"], msg_chan, exitchan)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	<-sigChan
+	for _, r := range reader_list {
 		r.Stop()
-		exitchan <- 1
-		log.Println("stop all")
 	}
+	close(exitchan)
+	close(msg_chan)
+	log.Println("stop all")
 	time.Sleep(time.Second)
+}
+
+func ReadConfig(file string) (map[string]string, error) {
+	var setting map[string]string
+	config_file, err := os.Open(file)
+	config, err := ioutil.ReadAll(config_file)
+	if err != nil {
+		return nil, err
+	}
+	defer config_file.Close()
+	if err := json.Unmarshal(config, &setting); err != nil {
+		return nil, err
+	}
+	return setting, nil
 }
 
 func tcp_server(port string, msg_chan chan Msg, exitchan chan int) {
