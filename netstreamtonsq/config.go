690b17346540f798732d296e399e23cce9520b13
datastream <guxianjie@gmail.com>
2014-03-13 16:22:42 +0800

    remove github.com/goinggo/mapstructure

--- netstreamtonsq/config.go
+++ netstreamtonsq/config.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/goinggo/mapstructure"
 	"io/ioutil"
 	"os"
 )
 
 // Config is metrictools config struct
 type Setting struct {
-	nsqdAddr      string `jpath:"nsqd_addr"`
-	topic         string `jpath:"topic"`
-	tcpPort       string `jpath:"tcp_listen_address"`
-	udpPort       string `jpath:"udp_listen_address"`
-	writePoolSize int    `jpath:"write_pool_size"`
+	nsqdAddr      string `json:"nsqd_addr"`
+	topic         string `json:"topic"`
+	tcpPort       string `json:"tcp_listen_address"`
+	udpPort       string `json:"udp_listen_address"`
+	writePoolSize int    `json:"write_pool_size"`
 }
 
 // ReadConfig used to read json to config
@@ -24,11 +23,9 @@ func ReadConfig(file string) (*Setting, error) {
 		return nil, err
 	}
 	configFile.Close()
-	docMap := make(map[string]interface{})
-	if err := json.Unmarshal(config, &docMap); err != nil {
+	setting := &Setting{}
+	if err := json.Unmarshal(config, setting); err != nil {
 		return nil, err
 	}
-	setting := &Setting{}
-	err = mapstructure.DecodePath(docMap, setting)
 	return setting, err
 }
