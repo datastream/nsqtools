1bac4ca8d33dc06b6df441f0dca064fb1a4517f7
datastream <guxianjie@gmail.com>
2015-04-28 15:40:48 +0800

    add auto file read

--- file_to_nsq/main.go
+++ file_to_nsq/main.go
@@ -1,113 +1,61 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/bitly/go-nsq"
-	"io"
-	"io/ioutil"
+	"gopkg.in/fsnotify.v1"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 var (
-	conf_file   = flag.String("conf", "config.json", "config file")
-	nsq_address = flag.String("nsq_address", "127.0.0.1:4150", "nsq")
+	fileDir    = flag.String("dir", "/tmp", "watch files")
+	nsqTopic   = flag.String("topic", "test#test", "nsq topic")
+	nsqAddress = flag.String("nsq_address", "127.0.0.1:4150", "nsq")
 )
 
 func main() {
 	flag.Parse()
 	cfg := nsq.NewConfig()
 	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal("fail to get hostname", err)
+	}
 	cfg.Set("user_agent", fmt.Sprintf("file_to_nsq/%s", hostname))
 	cfg.Set("snappy", true)
-	w, _ := nsq.NewProducer(*nsq_address, cfg)
-	setting, err := ReadConfig(*conf_file)
-	if err != nil {
-		log.Fatal("fail to read config", err)
-	}
+	w, _ := nsq.NewProducer(*nsqAddress, cfg)
 	exitchan := make(chan int)
-	for k, v := range setting {
-		go readLog(v, k, w, exitchan)
-	}
+	go watchFiles(*fileDir, *nsqTopic, w, exitchan)
 	termchan := make(chan os.Signal, 1)
 	signal.Notify(termchan, syscall.SIGINT, syscall.SIGTERM)
 	<-termchan
 	close(exitchan)
 }
 
-func ReadConfig(file string) (map[string]string, error) {
-	var setting map[string]string
-	config_file, err := os.Open(file)
-	config, err := ioutil.ReadAll(config_file)
+func watchFiles(fileDir string, topic string, w *nsq.Producer, exitchan chan int) {
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
-	defer config_file.Close()
-	if err := json.Unmarshal(config, &setting); err != nil {
-		return nil, err
-	}
-	return setting, nil
-}
-
-func readLog(file string, topic string, w *nsq.Producer, exitchan chan int) {
-	fd, err := os.Open(file)
+	defer watcher.Close()
+	var f FileList
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if f.Update(event) {
+					go f.ReadLog(event.Name, topic, w, exitchan)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+	err = watcher.Add(fileDir)
 	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer fd.Close()
-	_, err = fd.Seek(0, 2)
-	if err != nil {
-		return
-	}
-	reader := bufio.NewReader(fd)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			time.Sleep(time.Second * 10)
-			line, err = reader.ReadString('\n')
-		}
-		if err == io.EOF {
-			log.Println("READ EOF")
-			size0, err := fd.Seek(0, 1)
-			if err != nil {
-				return
-			}
-			fd, err = os.Open(file)
-			if err != nil {
-				log.Println("open failed", err)
-				return
-			}
-			size1, err := fd.Seek(0, 2)
-			if err != nil {
-				log.Println(err)
-			}
-			if size1 < size0 {
-				fd.Seek(0, 0)
-			} else {
-				fd.Seek(size0, 0)
-			}
-			reader = bufio.NewReader(fd)
-			continue
-		}
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		err = w.Publish(topic, []byte(line))
-		if err != nil {
-			log.Println("NSQ writer", err)
-		}
-		select {
-		case <-exitchan:
-			return
-		default:
-		}
+		log.Fatal(err)
 	}
 }
---
+++ file_to_nsq/watchfiles.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"github.com/bitly/go-nsq"
+	"gopkg.in/fsnotify.v1"
+	"io"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+type FileList struct {
+	Files map[string]bool
+	sync.Mutex
+}
+
+func (f *FileList) Update(e fsnotify.Event) bool {
+	switch e.Op {
+	case fsnotify.Remove:
+		f.Lock()
+		delete(f.Files, e.Name)
+		f.Unlock()
+	case fsnotify.Write:
+		if _, ok := f.Files[e.Name]; !ok {
+			f.Lock()
+			f.Files[e.Name] = true
+			f.Unlock()
+			return true
+		}
+	default:
+	}
+	return false
+}
+func (f *FileList) ReadLog(file string, topic string, w *nsq.Producer, exitchan chan int) {
+	fd, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer fd.Close()
+	_, err = fd.Seek(0, 2)
+	if err != nil {
+		return
+	}
+	reader := bufio.NewReader(fd)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			time.Sleep(time.Second * 10)
+			line, err = reader.ReadString('\n')
+		}
+		if err == io.EOF {
+			log.Println("READ EOF")
+			f.Lock()
+			if _, ok := f.Files[file]; !ok {
+				break
+			}
+			f.Unlock()
+			continue
+		}
+		err = w.Publish(topic, []byte(line))
+		if err != nil {
+			log.Println("NSQ writer", err)
+		}
+		select {
+		case <-exitchan:
+			return
+		default:
+		}
+	}
+}
